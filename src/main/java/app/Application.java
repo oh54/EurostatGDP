a9b87077475047f4f4ed4ed5da9c2a25ee0186e5
oskar <oskar.hint@gmail.com>
2016-09-02 13:01:34 +0200

    Modifications for heroku execution

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,24 +1,14 @@
 package app;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
+public class Application {
 
 	public static void main(String args[]) {
 		SpringApplication.run(Application.class);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-		
-		log.info("APP RUN");
-	}
 }
